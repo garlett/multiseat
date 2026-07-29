509269425d53e56a6ef932f15e56da070743c62f
Sturm0 <federicogerman@outlook.es>
2026-07-29 17:28:16 -0300

    Related to discussion #37

--- install_and_run.sh
+++ install_and_run.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
-pacman -S git
+pacman -S --needed git
 git clone -b wlroots-0.20 https://github.com/Sturm0/multiseat.git
 cd multiseat
-mv ./multiseat.sh /usr/local/bin/multiseat.sh
-mv ./login.sh 	  /usr/local/bin/login.sh
+cp ./multiseat.sh /usr/local/bin/multiseat.sh
+cp ./login/login.sh 	  /usr/local/bin/login.sh
+mkdir -p /usr/local/etc/multiseat/login
+cp ./login/alacritty.toml /usr/local/etc/multiseat/login/alacritty.toml
 chmod +x /usr/local/bin/multiseat.sh
 chmod +x /usr/local/bin/login.sh
+chmod 644 "/usr/local/etc/multiseat/login/alacritty.toml" # me aseguro que el usuario del "kiosco" pueda leerlo
+
 /usr/local/bin/multiseat.sh -a 
+
