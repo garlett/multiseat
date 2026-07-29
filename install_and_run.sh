ed8c773c8c9645ffb70828e245525d19f703bed5
Sturm0 <federicogerman@outlook.es>
2026-07-29 18:17:26 -0300

    Minor change

--- install_and_run.sh
+++ install_and_run.sh
@@ -6,6 +6,7 @@ cp ./multiseat.sh /usr/local/bin/multiseat.sh
 cp ./login/login.sh 	  /usr/local/bin/login.sh
 mkdir -p /usr/local/etc/multiseat/login
 cp ./login/alacritty.toml /usr/local/etc/multiseat/login/alacritty.toml
+cp ./login/autostart /usr/local/etc/multiseat/login/autostart
 chmod +x /usr/local/bin/multiseat.sh
 chmod +x /usr/local/bin/login.sh
 chmod 644 "/usr/local/etc/multiseat/login/alacritty.toml" # me aseguro que el usuario del "kiosco" pueda leerlo
